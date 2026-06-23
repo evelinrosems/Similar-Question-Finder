97eaa67a9178ac7526bf6f630c4e5bdea7170054
evelinrosems <evelinrosems@gmail.com>
2026-06-23 17:22:41 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
-
+const API_URL = 'https://similar-question-finder.onrender.com/api';
 const api = axios.create({
   baseURL: API_URL,
 });
